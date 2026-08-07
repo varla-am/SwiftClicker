c79b2f783478ac5e371488bea0e1dc096c1d39da
Varlaam <varlams@tarasovs.com>
2026-08-07 20:13:58 +0300

    Initial Commit

---
+++ SwiftClicker/SwiftClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftClickerApp.swift
+//  SwiftClicker
+//
+//  Created by Varlaam on 07/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
